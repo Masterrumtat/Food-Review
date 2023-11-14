71ac1bf09bc0410db776db91a5bdb663842d7177
Masterrumtat <captain019377@gmail.com>
2023-11-14 16:06:09 +0700

    uptodate

--- Food-Review/AppDelegate.swift
+++ Food-Review/AppDelegate.swift
@@ -6,6 +6,11 @@
 //
 
 import UIKit
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+// ...
+      
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +18,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        // Use Firebase library to configure APIs
         // Override point for customization after application launch.
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+          // ...
+        }
         return true
     }
 
